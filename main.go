a604b17f22a013ba52998d44da9ea224b8cc797f
Gabriel-Weiss <razlogpetru@gmail.com>
2024-09-08 18:41:06 +0300

    some changes
    Some fixes
    
    Added different pages

--- main.go
+++ main.go
@@ -7,12 +7,19 @@ import (
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	app := fiber.New()
-	app.Get("/", adaptor.HTTPHandler(templ.Handler(pages.IndexTempl("Hello world!!!"))))
+	app.Use(cors.New())
+	app.Static("/", "./assets")
+	app.Get("/", adaptor.HTTPHandler(templ.Handler(pages.IndexPage("Hello world!!!"))))
+	app.Get("/shop", adaptor.HTTPHandler(templ.Handler(pages.ShopPage("Hello world!!!"))))
+	app.Get("/categories", adaptor.HTTPHandler(templ.Handler(pages.CategoriesPage("Hello world!!!"))))
+	app.Get("/contact", adaptor.HTTPHandler(templ.Handler(pages.ContactPage("Hello world!!!"))))
+	app.Get("/about", adaptor.HTTPHandler(templ.Handler(pages.AboutPage("Hello world!!!"))))
 
 	log.Fatal(app.Listen(":3000"))
 }
